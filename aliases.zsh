f7d39f9fcc0f515ee9e824fa593a7a7e768799c7
fenec <fenec230@yahoo.com>
2013-07-10 03:43:19 +0400

    zsh aliases added

---
+++ aliases.zsh
@@ -0,0 +1,23 @@
+# Git
+alias gc="git commit"
+alias gca="git commit -a"
+alias gch="git checkout"
+alias gp="git pull"
+alias gpu="git push"
+alias gs="git status"
+alias gd="git diff"
+alias gf="git fetch"
+alias gb="git branch"
+alias gst="git stash"
+alias ga="git add"
+alias gaa="git add -A"
+
+# Rails
+alias r="rails"
+
+alias rr="rake routes"
+alias rdm="rake db:migrate"
+
+
+
+
