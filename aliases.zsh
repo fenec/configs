ce4cb7eed87302890121b90b4fbc68a77ed59429
fenec <fenec230@yahoo.com>
2013-07-10 13:20:20 +0400

    updates to aliases, rake and git

--- aliases.zsh
+++ aliases.zsh
@@ -1,4 +1,5 @@
 # Git
+alias g="git"
 alias gc="git commit"
 alias gca="git commit -a"
 alias gch="git checkout"
@@ -9,15 +10,17 @@ alias gd="git diff"
 alias gf="git fetch"
 alias gb="git branch"
 alias gst="git stash"
+alias gsp="git stash pop"
 alias ga="git add"
 alias gaa="git add -A"
+alias gm="git merge"
 
 # Rails
 alias r="rails"
 
 alias rr="rake routes"
 alias rdm="rake db:migrate"
-
+alias rdtp="rake db:test:prepare"
 
 
 
