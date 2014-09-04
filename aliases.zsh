dd753458f724abd3f9f8df6aa2e6b8c226ad6cab
Fenec <fenec230@yahoo.com>
2014-09-04 13:00:14 +0400

    Update aliases.zsh

--- aliases.zsh
+++ aliases.zsh
@@ -1,30 +1,38 @@
 # Git
 alias g="git"
-alias gc="git commit"
-alias gca="git commit -a"
-alias gch="git checkout"
-alias gp="git pull --rebase"
-alias gpu="git push"
 alias gs="git status"
 alias gd="git diff"
-alias gf="git fetch"
-alias gb="git branch"
-alias gst="git stash"
-alias gsp="git stash pop"
+alias gdh="git diff HEAD"
+
 alias ga="git add"
 alias gaa="git add -A"
 alias gau="git add -u"
+
+alias gc="git commit"
+alias gca="git commit -a"
+alias gcam="git commit --amend"
+
+alias gf="git fetch"
+alias gp="git pull --rebase"
+alias gpu="git push"
 alias gm="git merge"
-alias gdb="git delete-branch"
+
+alias gch="git checkout"
+alias gst="git stash"
+alias gsp="git stash pop"
 alias gss="git stash show -p"
-alias grm="git rebase master"
-alias grmc="git rm -r --cached ."
-alias gr="git rebase"
+
+alias gb="git branch"
 alias gchb="git checkout -b $1"
-alias grc="git rebase --continue"
-alias grH="git reset --hard"
+alias gdb="git delete-branch"
+
 alias gr="git rebase"
-alias gdh="git diff HEAD"
+alias grm="git rebase master"
+alias grc="git rebase --continue"
+
+alias grH="git reset --hard"
+alias grmc="git rm -r --cached ."
+
 
 alias bi="bundle install"
 
