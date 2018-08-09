f2fac6e6af1f09158495c040281f71cc0267854b
fenec <fenec230@yahoo.com>
2018-08-09 09:51:25 +0200

    Update zsh aliases

--- aliases.zsh
+++ aliases.zsh
@@ -3,13 +3,16 @@ alias g="git"
 alias gs="git status"
 alias gd="git diff"
 alias gdh="git diff HEAD"
+alias gdhh="git diff HEAD~1"
+alias gst="git stash"
+alias glg="git log --stat"
 
 alias ga="git add"
 alias gaa="git add -A"
 alias gau="git add -u"
 
 alias gc="git commit"
-alias gca="git commit -a"
+alias gcA="git commit -a"
 alias gcam="git commit --amend"
 
 alias gf="git fetch"
@@ -18,9 +21,12 @@ alias gpu="git push"
 alias gm="git merge"
 
 alias gch="git checkout"
+alias gchH="git checkout HEAD"
+alias gcd="git checkout develop"
 alias gst="git stash"
-alias gsp="git stash pop"
+alias gsP="git stash pop"
 alias gss="git stash show -p"
+alias gsu="git stash -u"
 
 alias gb="git branch"
 alias gchb="git checkout -b $1"
@@ -29,9 +35,16 @@ alias gdb="git delete-branch"
 alias gr="git rebase"
 alias grm="git rebase master"
 alias grc="git rebase --continue"
+alias gri2="git rebase -i HEAD~2"
 
 alias grH="git reset --hard"
+alias grs="git reset"
 alias grmc="git rm -r --cached ."
+
+alias ggpushF="ggpush --force-with-lease"
+
+# Git Flow
+alias gfs="git flow feature start"
 
 alias gx="gitx --all"
 
@@ -45,10 +58,14 @@ alias psgrep='ps aux | grep $(echo $1 | sed "s/^\(.\)/[\1]/g")'
 alias r="rails"
 alias rs="rails s"
 
+# Docker
+alias dk="docker"
+
 # Rspec
 alias rsp="rspec spec --tag=focus"
 
 alias rr="rake routes"
 alias rdm="rake db:migrate && rake db:migrate RAILS_ENV=test"
-alias rdr="rake db:rollback"
+alias rdr="RAILS_ENV=test rake db:rollback && rake db:rollback"
 alias rds="rake db:seed"
+
